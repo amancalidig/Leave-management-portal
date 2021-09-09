860e8626789edb48efb0fe29408e894a64cc160e
amansharma1020 <aman.sharma@calidig.com>
2021-09-09 14:39:46 +0530

    first commit

--- src/app/front-page/dashboard/dashboard.component.ts
+++ src/app/front-page/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  user: any = JSON.parse(sessionStorage.getItem('user') || 'null');
+  
+  ngOnInit(): void {}
 }
